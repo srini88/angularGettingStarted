84a3e0be11333c35fe7456a9038030bdf8fc818d
srini88 <pvsrinivas.ece@gmail.com>
2016-01-14 18:27:01 -0600

    get myUserInfo from gitapi using http service, promise

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('myController', ['$scope', function ($scope){
+myApp.controller('myController', ['$scope','$http', function ($scope, $http){
 
+	var onUserComplete = function(response){
+		$scope.user = response.data;
+	};
 
-	var person = {
-		firstName : "Srinivas",
-		lastName : "Palghat"
-	}
-
-
+	var onError = function(response){
+		$scope.error = "Could not fetch the user";
+	};
+	// telling then to call onUserComplete when http request is successful
+	$http.get("https://pi.github.com/users/srini88")
+		 .then(onUserComplete, onError);  //then is only invoked if the call is successful 
 	$scope.message = "Srinivas is gonna be a top notch angular developer";
-	$scope.person = person;
+
 }]);
 
